6eb58be2331b0370016586b4fd60b7a106a36c99
7h4ztmv78w-rgb <250653162+7h4ztmv78w-rgb@users.noreply.github.com>
2026-06-15 21:31:50 +0900

    Build lift log PWA and iOS wrapper

---
+++ sw.js
@@ -0,0 +1,34 @@
+const cacheName = "lift-log-v6";
+const assets = [
+  "./",
+  "./index.html",
+  "./styles.css?v=6",
+  "./app.js?v=6",
+  "./manifest.webmanifest?v=6",
+  "./icon.svg",
+  "./icon-192.png",
+  "./icon-512.png",
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(caches.open(cacheName).then((cache) => cache.addAll(assets)));
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => Promise.all(keys.filter((key) => key !== cacheName).map((key) => caches.delete(key))))
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  if (event.request.method !== "GET") return;
+  event.respondWith(
+    fetch(event.request)
+      .then((response) => {
+        const copy = response.clone();
+        caches.open(cacheName).then((cache) => cache.put(event.request, copy));
+        return response;
+      })
+      .catch(() => caches.match(event.request))
+  );
+});
