7365866c9fe843d0accfecd803098e50d3ad48af
7h4ztmv78w-rgb <250653162+7h4ztmv78w-rgb@users.noreply.github.com>
2026-06-15 23:48:48 +0900

    Allow saving planned empty sets

--- www/sw.js
+++ www/sw.js
@@ -1,34 +1,29 @@
-const cacheName = "lift-log-v6";
-const assets = [
+const cacheName = "lift-log-v7";
+const appShell = [
   "./",
   "./index.html",
-  "./styles.css?v=6",
-  "./app.js?v=6",
-  "./manifest.webmanifest?v=6",
-  "./icon.svg",
+  "./manifest.webmanifest?v=7",
+  "./styles.css?v=7",
+  "./app.js?v=7",
   "./icon-192.png",
   "./icon-512.png",
 ];
 
 self.addEventListener("install", (event) => {
-  event.waitUntil(caches.open(cacheName).then((cache) => cache.addAll(assets)));
+  event.waitUntil(caches.open(cacheName).then((cache) => cache.addAll(appShell)));
+  self.skipWaiting();
 });
 
 self.addEventListener("activate", (event) => {
   event.waitUntil(
-    caches.keys().then((keys) => Promise.all(keys.filter((key) => key !== cacheName).map((key) => caches.delete(key))))
+    caches
+      .keys()
+      .then((keys) => Promise.all(keys.filter((key) => key !== cacheName).map((key) => caches.delete(key))))
+      .then(() => self.clients.claim())
   );
 });
 
 self.addEventListener("fetch", (event) => {
   if (event.request.method !== "GET") return;
-  event.respondWith(
-    fetch(event.request)
-      .then((response) => {
-        const copy = response.clone();
-        caches.open(cacheName).then((cache) => cache.put(event.request, copy));
-        return response;
-      })
-      .catch(() => caches.match(event.request))
-  );
+  event.respondWith(fetch(event.request).catch(() => caches.match(event.request).then((response) => response || caches.match("./index.html"))));
 });
